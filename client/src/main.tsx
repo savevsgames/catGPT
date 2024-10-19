d7839a43fbdea2a1454bb9684992625861640f8a
DarioJZB <dariojzb87@gmail.com>
2024-10-19 18:42:35 -0400

    front end started

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import '/src/index.css';
+import Welcome from './pages/welcome.tsx'
+import Signup from './pages/signup.tsx'
+
+const router = createBrowserRouter ([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Welcome />,
+      },
+      {
+        path: '/Signup',
+        element: <Signup />,
+      },
+      // {
+      //   path: '/Profile',
+      //   element: <Profile />,
+      // },
+      // {
+      //   path: '/Cat',
+      //   element: <Cat />,       
+      // },
+      // {
+      //   path: '/Error',
+      //   element: <Error />,
+      // },   
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
+
+export default router;
