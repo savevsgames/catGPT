4b1b68ace6d44f4dcd307b0f04acf292f6567cd7
Thomas Stemler <stemlertho@gmail.com>
2024-10-21 17:07:46 -0500

    Finished Routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,48 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {BrowserRouter, } from 'react-router-dom';
 import App from './App.tsx';
 import '/src/index.css';
-import Landing from './pages/Landing.tsx'
-import Signup from './pages/Signup.tsx'
 
-const router = createBrowserRouter ([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-      },
-      {
-        path: '/Signup',
-        element: <Signup />,
-      },
-      // {
-      //   path: '/Login',
-      //   element: <Login />,
-      // },
-      // {
-      //   path: '/Profile',
-      //   element: <Profile />,
-      // },
-      // {
-      //   path: '/Cat',
-      //   element: <Cat />,       
-      // },
-      // {
-      //   path: '/Error',
-      //   element: <Error />,
-      // },   
-    ],
-  },
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    {/*<RouterProvider router={router} />*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
 
-export default router;
+// export default router;
