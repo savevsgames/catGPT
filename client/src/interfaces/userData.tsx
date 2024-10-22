734366935e017ccaabd9e47a2b6e99e5e99e9e04
Souad-HB <souadsalahh@gmail.com>
2024-10-22 08:16:37 -0400

    userAPIclientSide

---
+++ client/src/interfaces/userData.tsx
@@ -0,0 +1,9 @@
+export interface UserData {
+    id: number | null;
+    username: string | null;
+    email: string | null;
+    userRole: string | null;
+    bio: string | null;
+    yarn: number | null;
+
+}
