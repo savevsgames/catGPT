162b3b7ff973c4c801206d1681a7b059a0ed749f
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-21 19:52:25 -0600

    Css for landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from "react";
+import { Link } from "react-router-dom";
 
-const Landing: React.FC = () => {
-    return (
-        <div className='bg-gray-400 min-h-screen'>
-            <Link to="/Signup" className='p-4  flex flex-col items-center rounded-bl rounded-br'>
-                <h1 className='text-5xl pt-6'>Please adopt me</h1>
-                <a className='p-3'>click/tap here to start your journey as my new pawrent</a>
-                <img src='src/assets/adoptMe.png' className="h-1/2 w-1/2"></img>
-            </Link>
-        </div>
-    );
+const catApiPhotos = [
+  "assets/adoptMe.png",
+  "assets/adoptMe.png",
+  "https://placekitten.com/300/300",
+  "https://placekitten.com/350/250",
+];
+
+// Create two random functions to generate random rotation and position for the cat photos
+const randomRotation = () => Math.floor(Math.random() * 21) - 10; // Random angle between -10 and 10 degrees
+const randomPosition = () => `${Math.floor(Math.random() * 80)}%`; // Random position between 0-80%
+
+const Landing = () => {
+  return (
+    <div className="relative bg-yellow-100 min-h-screen p-10 flex justify-center items-center">
+      <div className="relative w-full max-w-5xl h-[600px] bg-[#d7b899] rounded-lg shadow-lg overflow-hidden">
+        {/* Render the cat photos randomly on the board */}
+        {catApiPhotos.map((photo, index) => (
+          <Link
+            to="/Signup"
+            className="p-4  flex flex-col items-center rounded-bl rounded-br"
+          >
+            <img
+              key={index}
+              src={photo}
+              alt={`Cat`}
+              className="absolute w-40 h-40 object-cover rounded shadow-lg"
+              style={{
+                transform: `rotate(${randomRotation()}deg)`,
+                top: randomPosition(),
+                left: randomPosition(),
+              }}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
