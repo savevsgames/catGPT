4fc473cea5cd7c8205a2ee56fd3edc9ac0a6ad16
Thomas Stemler <stemlertho@gmail.com>
2024-10-22 19:26:21 -0500

    Made the positions be defined in the props rather than upon element creation

--- client/src/components/catPolaroid.tsx
+++ client/src/components/catPolaroid.tsx
@@ -2,38 +2,39 @@ import {Link} from "react-router-dom";
 
 
 const randomRotation = () => Math.floor(Math.random() * 21) - 10; // Random angle between -10 and 10 degrees
-const randomPosition = () => `${Math.floor(Math.random() * 80)}%`; // Random position between 0-80%
 
 
 interface Props {
-    index: number;
-    photoURL: string;
+  index: number;
+  photoURL: string;
+  topPercent: string | "50%";
+  leftPercent: string | "50%";
 }
 
 function CatPolaroid(props: Props) {
-    return (
-        <Link
-            to="/Signup"
-            className="p-4  flex flex-col items-center rounded-bl rounded-br"
-        >
-            <img
-                key={props.index}
-                src={props.photoURL}
-                alt={`Cat`}
-                className="absolute w-40 h-40 object-cover rounded shadow-lg"
-                style={{
-                    transform: `rotate(${randomRotation()}deg)`,
-                    top: randomPosition(),
-                    left: randomPosition(),
-                    paddingRight: '10px',
-                    paddingLeft: '10px',
-                    paddingTop: '10px',
-                    paddingBottom: '45px',
-                    backgroundColor: `#FFF7ED`,
-                }}
-            />
-        </Link>
-    )
+  return (
+    <Link
+      to="/Signup"
+      className="p-4  flex flex-col items-center rounded-bl rounded-br"
+    >
+      <img
+        key={props.index}
+        src={props.photoURL}
+        alt={`Cat`}
+        className="absolute w-40 h-40 object-cover rounded shadow-lg"
+        style={{
+          transform: `rotate(${randomRotation()}deg)`,
+          top: props.topPercent,
+          left: props.leftPercent,
+          paddingRight: '10px',
+          paddingLeft: '10px',
+          paddingTop: '10px',
+          paddingBottom: '45px',
+          backgroundColor: `#FFF7ED`,
+        }}
+      />
+    </Link>
+  )
 }
 
 export default CatPolaroid;
