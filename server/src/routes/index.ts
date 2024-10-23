93952bf65431afc2bf8b94245ddccbaacc8892e8
Souad-HB <souadsalahh@gmail.com>
2024-10-23 17:37:29 -0400

    arranged wrong files and file names

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import apiRoutes from "./api/index.js";
+import authRoutes from './auth-routes.js';
+import { authenticateToken } from "../middleware/auth.js";
 
 const router = Router();
 
-// right now we access the routes without auth. the middleware will be added later
-router.use("/api", apiRoutes);
+router.use("/auth", authRoutes);
+// uncomment line 10 if you want to continue without auth for testing and comment out line 12.
+// router.use("/api", apiRoutes);
+// added middleware for authetication for all apiRoutes.
+router.use("/api", authenticateToken, apiRoutes);
 
 export default router;
