e4adf86ee1c1791198754b39568c0a56c8107289
DarioJZB <dariojzb87@gmail.com>
2024-10-28 10:36:33 -0400

    App bug fix

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -87,57 +87,5 @@ function NavBar() {
         </nav>
     );
 };
-  return (
-    <nav className="bg-color_2 w-full flex p-4 top-0 left-0">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="flex items-center space-x-2">
-          <img
-            src="assets/other/logo.png"
-            alt="Cat Logo"
-            className="h-10 w-30"
-          />
-        </Link>
-        <div>
-          {loggedIn ? (
-            <>
-              <button
-                className="ml-2 px-4 py-2 bg-color_3 rounded-lg hover:bg-color_5 transition-colors"
-                onClick={handleCatsNav}
-              >
-                Cats
-              </button>
-              <button
-                className="ml-2 px-4 py-2 bg-color_3 rounded-lg hover:bg-color_5 transition-colors"
-                onClick={handleProfileNav}
-              >
-                Profile
-              </button>
-              <button
-                className="ml-2 px-4 py-2 bg-color_3 rounded-lg hover:bg-color_5 transition-colors"
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-            <button
-                        className="ml-2 px-4 py-2 bg-color_3 rounded-lg hover:bg-color_5 transition-colors"
-                        onClick={handleSignup}>
-                                Signup
-                      </button>
-            <button
-              className="ml-2 px-4 py-2 bg-color_3 rounded-lg hover:bg-color_5 transition-colors"
-              onClick={handleLogin}
-            >
-              Login
-            </button>
-             <button className="bg-yellow-500 text-black px-4 py-2 rounded" onClick={handleLogin}>Login</button>
-            </>
-        </div>
-      </div>
-    </nav>
-  );
-}
 
 export default NavBar;
