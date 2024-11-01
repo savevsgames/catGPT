6519aa956fc0e5fb51ed4c740f436fe0daff0bbc
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-31 20:14:21 -0600

    updated build steps

---
+++ client/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+export default defineConfig({
+    server: {
+        port: 5173, // Adjusted for Vite dev server
+        open: true, // Automatically open the browser on startup
+        proxy: {
+            "/api": {
+                target: "http://localhost:3001", // Express backend server
+                changeOrigin: true,
+                secure: false, // HTTPS?
+            },
+            "/auth": {
+                target: "http://localhost:3001", // Proxy auth routes to Express
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+    plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
+});
