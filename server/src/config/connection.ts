6557a20d019cb12c917f95e58b5f5ae3aebc8d88
savevsgames <126988787+savevsgames@users.noreply.github.com>
2025-02-13 23:19:59 -0700

    sequelize connection updates for ssl

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,18 +4,28 @@ dotenv.config();
 import { Sequelize } from "sequelize";
 
 // create sequelize connection
-
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          // rejectUnauthorized: false, // Use with caution
+        },
+      },
+    })
   : new Sequelize(
       process.env.DB_NAME || "",
       process.env.DB_USER || "",
       process.env.DB_PASSWORD || "",
       {
-        host: "localhost",
+        host: process.env.DB_HOST || "localhost",
         dialect: "postgres",
         dialectOptions: {
-          decimalNumbers: true,
+          ssl: {
+            require: true,
+            // rejectUnauthorized: false, // Use with caution
+          },
         },
       }
     );
